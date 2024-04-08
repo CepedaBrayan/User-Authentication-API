fa31d0cbe50c6699f24bd9a208bd85f8a21e6ec7
Brayan Rivera Cepeda <44098760+CepedaBrayan@users.noreply.github.com>
2024-04-08 07:43:26 -0500

    cleaning comments

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -22,22 +22,14 @@ export class AuthGuard implements CanActivate {
       context.getHandler(),
       context.getClass(),
     ]);
-    if (isPublic) {
-      // 💡 See this condition
-      return true;
-    }
-
+    if (isPublic) return true;
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
-    }
+    if (!token) throw new UnauthorizedException();
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
